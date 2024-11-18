89d21dc80e7184b4995ee9b2b160b8191556f233
GrigDavid <MyEmail>
2024-11-18 22:41:48 +0400

    feat: Added new theme

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,8 @@ module.exports = {
     rules: {
         "react/jsx-indent": [2, 4],
         "react/jsx-indent-props": [2, 4],
-        indent: [2, 4],
+        // indent: [2, 4],
+        indent: "off",
         "react/jsx-filename-extension": [
             2,
             { extensions: [".js", ".jsx", ".tsx"] },
